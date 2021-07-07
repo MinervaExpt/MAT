ef108b5baca681657b59f8651b342e82ee6ad773
Andrew Olivier <andrew@djoinc.com>
2021-07-07 15:38:46 -0500

    Had to add MnvH3D back in to compile UnfoldUtils.

--- dict/PlotUtilsDict.h
+++ dict/PlotUtilsDict.h
@@ -22,11 +22,14 @@
 #include "../PlotUtils/HistogramUtils.h"
 #include "../PlotUtils/MnvH1D.h"
 #include "../PlotUtils/MnvH2D.h"
+#include "../PlotUtils/MnvH3D.h"
 #include "../PlotUtils/MnvLatErrorBand.h"
 #include "../PlotUtils/MnvLatErrorBand2D.h"
+#include "../PlotUtils/MnvLatErrorBand3D.h"
 #include "../PlotUtils/MnvPlotter.h"
 #include "../PlotUtils/MnvVertErrorBand.h"
 #include "../PlotUtils/MnvVertErrorBand2D.h"
+#include "../PlotUtils/MnvVertErrorBand3D.h"
 #include "../PlotUtils/ChainWrapper.h"
 #include "../PlotUtils/TreeWrapper.h"
 #include "../PlotUtils/GridCanvas.h"
@@ -48,6 +51,8 @@ template class std::pair<std::string, std::vector<std::string> >;
 // The std::pair<>s for those std::map<>s don't seem to be generated though.
 template class std::pair< std::string, PlotUtils::MnvLatErrorBand* >;
 template class std::pair< std::string, PlotUtils::MnvVertErrorBand* >;
+template class std::pair< std::string, PlotUtils::MnvVertErrorBand3D* >;
+template class std::pair< std::string, PlotUtils::MnvLatErrorBand3D* >;
 template class std::pair< std::string, TMatrixT<double>* >;
 template class std::pair< std::string, PlotUtils::MnvVertErrorBand2D* >;
 template class std::pair< std::string, PlotUtils::MnvLatErrorBand2D* >;
