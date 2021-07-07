6e6d56be34909337d87fca93f826dd7d5dedaf68
Andrew Olivier <andrew@djoinc.com>
2021-07-07 14:12:32 -0500

    Started removing classes that have moved to MAT-MINERvA.

--- dict/PlotUtilsDict.h
+++ dict/PlotUtilsDict.h
@@ -18,9 +18,6 @@
 
 #include <vector>
 
-#include "../PlotUtils/NSFDefaults.h"
-#include "../PlotUtils/AnaBinning.h"
-#include "../PlotUtils/ArachneUtils.h"
 #include "../PlotUtils/Exceptions.h"
 #include "../PlotUtils/HistogramUtils.h"
 #include "../PlotUtils/MnvFluxConstraint.h" 
@@ -48,29 +45,8 @@
 #include "../PlotUtils/MnvColors.h"
 #include "../PlotUtils/GridCanvas.h"
 
-// PlotUtils weight classes
-#include "../PlotUtils/weightRPA.h"
-#include "../PlotUtils/weight_2p2h.h"
-#include "../PlotUtils/weightLowQ2Pi.h"
-#include "../PlotUtils/weightDIS.h"
-#include "../PlotUtils/weightZExp.h"
-
 //PlotUtils systematic universes classes (new sys framework)
-#include "../PlotUtils/DefaultCVUniverse.h"
 #include "../PlotUtils/MinervaUniverse.h"
-//#include "../PlotUtils/HistWrapper.h"
-//#include "../PlotUtils/Hist2DWrapper.h"
-#include "../PlotUtils/FluxSystematics.h"
-#include "../PlotUtils/GenieSystematics.h"
-#include "../PlotUtils/GeantHadronSystematics.h"
-#include "../PlotUtils/MnvTuneSystematics.h"
-#include "../PlotUtils/MinosEfficiencySystematics.h"
-#include "../PlotUtils/MuonSystematics.h"
-#include "../PlotUtils/MuonResolutionSystematics.h"
-#include "../PlotUtils/MichelSystematics.h"
-#include "../PlotUtils/AngleSystematics.h"
-#include "../PlotUtils/ResponseSystematics.h"
-
 
 //TODO: Do I need this?
 #include "../PlotUtils/ErrorHandler.h"
@@ -86,27 +62,6 @@ template class std::pair<std::string, std::vector<PlotUtils::MnvEVD::Event> >;  
 template class std::map<std::string, std::vector<std::string> >;
 template class std::pair<std::string, std::vector<std::string> >;
 
-template class PlotUtils::BeamAngleXUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::BeamAngleYUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::FluxUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::GenieUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::GenieRvx1piUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::GenieFaCCQEUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::ResponseUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::MinosEfficiencyUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::MuonAngleXResolutionUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::MuonAngleYResolutionUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::MuonResolutionUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::MichelEfficiencyUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::LowQ2PionUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::MuonUniverseMinerva<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::MuonUniverseMinos<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::RPAUniverse<PlotUtils::DefaultCVUniverse>;
-template class PlotUtils::Universe2p2h<PlotUtils::DefaultCVUniverse>;
-
-//template class PlotUtils::HistWrapper<PlotUtils::DefaultCVUniverse>;
-//template class PlotUtils::Hist2DWrapper<PlotUtils::DefaultCVUniverse>;
-
 // The std::pair<>s for those std::map<>s don't seem to be generated though.
 template class std::pair< std::string, PlotUtils::MnvLatErrorBand* >;
 template class std::pair< std::string, PlotUtils::MnvVertErrorBand* >;
@@ -115,20 +70,6 @@ template class std::pair< std::string, PlotUtils::MnvVertErrorBand2D* >;
 template class std::pair< std::string, PlotUtils::MnvLatErrorBand2D* >;
 template class std::pair< std::string, PlotUtils::MnvVertErrorBand3D* >;
 template class std::pair< std::string, PlotUtils::MnvLatErrorBand3D* >;
-
-// Use extern keyword because these functions are instantiated in FluxReweighter.cxx already
-extern template void PlotUtils::FluxReweighter::AddFluxErrorBand<PlotUtils::MnvH1D>(PlotUtils::MnvH1D*);
-extern template void PlotUtils::FluxReweighter::AddFluxErrorBand<PlotUtils::MnvH2D>(PlotUtils::MnvH2D*);
-extern  template MnvH1D* FluxReweighter::GetIntegratedFluxReweighted<MnvH1D>( int nuPDG,
-                                                                 MnvH1D* template_hist,
-                                                                 double min_energy,
-                                                                 double max_energy,
-                                                                 bool use_muon_correlations);
-extern template MnvH2D* FluxReweighter::GetIntegratedFluxReweighted<MnvH2D>( int nuPDG,
-                                                                 MnvH2D* template_hist,
-                                                                 double min_energy,
-                                                                 double max_energy,
-                                                                 bool use_muon_correlations);
 
 #endif
 #endif // PLOTUTILSDICT_H
