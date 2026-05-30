664055af46f448767d92e9e6fb6474032aef542d
cpernas <cpernas@wm.edu>
2026-05-30 11:24:00 -0500

    Updating this outdated .rootlogon.C with the version that we previously provided in the tutorial wiki. This is only necessary to write ROOT macros that include MAT or MAT-MINERvA headers, so if you do need to do that make sure you copy this file into your home directory (or any other path on $ROOT_INCLUDE_PATH) so that it can load automatically

--- setup/.rootlogon.C
+++ setup/.rootlogon.C
@@ -1,16 +1,14 @@
 {
   if( gSystem->Getenv("PLOTUTILSROOT") )
   {
-    gInterpreter->AddIncludePath( gSystem->ExpandPathName("$PLOTUTILSROOT") );
-    string newpath = string(gROOT->GetMacroPath()) + ":" + string(gSystem->ExpandPathName("$PLOTUTILSROOT")) + "/PlotUtils";
+    string newpath = string(gROOT->GetMacroPath()) + ":" + string("${PLOTUTILSROOT}/../bin" );
     gROOT->SetMacroPath( newpath.c_str() );
-
-    gSystem->Load( "libCintex.so" );  // needed to process the dictionaries for the objects
-    Cintex::Enable();
-    gSystem->Load( gSystem->ExpandPathName("$PLOTUTILSROOT/$CMTCONFIG/libplotutils.so") );
-
-    //gInterpreter->ExecuteMacro("PlotStyle.C");
+    gInterpreter->AddIncludePath( "${PLOTUTILSROOT}/../include" );
+    gInterpreter->AddIncludePath( "${PLOTUTILSROOT}/../include/PlotUtils" );
+    std::vector<std::string> packages = { "MAT", "MAT-MINERvA" };
+    for(const std::string& package: packages)
+    {
+      gSystem->Load( gSystem->ExpandPathName(("$PLOTUTILSROOT/lib" + package + ".so").c_str()) );
+    }
   }
-
 }
-
